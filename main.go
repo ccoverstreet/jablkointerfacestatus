4488a7f02b3857754301d3120f8980a32acfd565
ccoverstreet <coverst2@vols.utk.edu>
2020-10-06 21:21:51 -0400

    Added handler

--- main.go
+++ main.go
@@ -3,8 +3,13 @@ package jablkointerfacestatus
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func Initialize(input string) {
 	fmt.Printf("INTERFACE STATUS\n")
+}
+
+func WebHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("High\n")
 }
